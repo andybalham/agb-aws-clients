da8369570597222fee7f651636f5c9bc39f9b783
Andy Blackledge <andyblackledge@hotmail.com>
2021-02-28 16:18:05 +0000

    Linting fixes

--- src/Log.ts
+++ src/Log.ts
@@ -1,4 +1,5 @@
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable import/prefer-default-export */
 export class Log {
   debug?: (message: string, params?: Record<string, any>) => void;
 
