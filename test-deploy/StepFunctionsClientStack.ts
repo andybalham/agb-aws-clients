21bdf534610da3728d11fd3382752c6178e61e76
Andy Blackledge <andyblackledge@hotmail.com>
2021-03-03 20:51:59 +0000

    Now with a test stack for StepFunctionsClient

--- test-deploy/StepFunctionsClientStack.ts
+++ test-deploy/StepFunctionsClientStack.ts
@@ -5,6 +5,9 @@
 /* eslint-disable no-new */
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as sfn_tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import * as logs from '@aws-cdk/aws-logs';
 
 export default class StepFunctionsClientStack extends cdk.Stack {
   //
@@ -12,10 +15,34 @@ export default class StepFunctionsClientStack extends cdk.Stack {
     //
     super(scope, id, props);
 
-    new lambda.Function(this, 'HelloHandler', {
+    const logInputFunction = new lambda.Function(this, 'LogInputFunction', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.fromAsset('dist/test-deploy/functions'),
-      handler: 'StepFunctionsClientFunctions.startExecutionHandler',
+      code: lambda.Code.fromAsset('dist'),
+      handler: 'test-deploy/functions/StepFunctionsClientFunctions.logInputHandler',
     });
+
+    const stateMachine = new sfn.StateMachine(this, 'StateMachine', {
+      stateMachineType: sfn.StateMachineType.EXPRESS,
+      definition: sfn.Chain.start(
+        new sfn_tasks.LambdaInvoke(this, 'LogInput', {
+          lambdaFunction: logInputFunction,
+        })
+      ),
+      logs: {
+        destination: new logs.LogGroup(this, 'StateMachineLogGroup'),
+        level: sfn.LogLevel.ALL,
+      },
+    });
+
+    const startStateMachineFunction = new lambda.Function(this, 'StartStateMachine', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('dist'),
+      handler: 'test-deploy/functions/StepFunctionsClientFunctions.startExecutionHandler',
+      environment: {
+        STATE_MACHINE_ARN: stateMachine.stateMachineArn,
+      },
+    });
+
+    stateMachine.grantStartExecution(startStateMachineFunction);
   }
 }
--- test-deploy/functions/StepFunctionsClientFunctions.ts
+++ test-deploy/functions/StepFunctionsClientFunctions.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
-
 import { StepFunctionsClient } from '../../src';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,4 +9,10 @@ export const startExecutionHandler = async (event: any): Promise<void> => {
   const stepFunctionsClient = new StepFunctionsClient(process.env.STATE_MACHINE_ARN);
 
   await stepFunctionsClient.startExecutionAsync(event);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const logInputHandler = async (input: any): Promise<void> => {
+  //
+  console.log(`input: ${JSON.stringify(input)}`);
 };
